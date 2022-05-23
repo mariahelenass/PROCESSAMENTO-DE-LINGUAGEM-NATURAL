2f33c0015d0b4b7414c5683dcb3f2ab17c0c92f2
Maria Helena <90515368+mariahelenass@users.noreply.github.com>
2022-05-23 16:01:20 -0300

    Add files via upload

---
+++ LISTAS .py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Criando uma lista
+
+
+# In[1]:
+
+
+mylist = ["a", "b", "c"]
+
+
+# In[2]:
+
+
+mylist
+
+
+# In[4]:
+
+
+mylist2 = [1,2,3,4,5]
+
+
+# In[5]:
+
+
+mylist2
+
+
+# In[6]:
+
+
+#procurando elementos na lista
+mylist[0]
+
+
+# In[7]:
+
+
+mylist2[0]
+
+
+# In[8]:
+
+
+#usando intervalo de indices
+mylist2[0:2]
+
+
+# In[9]:
+
+
+mylist[:2]
+
+
+# In[10]:
+
+
+mylist[:3]
+
+
+# In[11]:
+
+
+#pegando os dois ultimos da lista
+mylist[-2:]
+
